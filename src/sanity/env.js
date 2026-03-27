ac3f986a82ee6445fa90162bc7e93ffffece34d4
James Holt <james.holt@hopeforjustice.org>
2026-03-27 15:41:49 +0000

    ignoring dist

--- src/sanity/env.js
+++ src/sanity/env.js
@@ -1,5 +1,6 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-12-11'
+	process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-12-11";
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'yriten1b';
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+export const projectId =
+	process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "yriten1b";
